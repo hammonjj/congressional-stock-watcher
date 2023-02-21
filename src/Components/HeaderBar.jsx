5a3dcd1ec5215dfc63974fe517b14431837119f8
hammonjj <james.hammond@thegruppetto.com>
2023-02-21 11:21:48 -0700

    Updated for blank disclosures

--- src/Components/HeaderBar.jsx
+++ src/Components/HeaderBar.jsx
@@ -1,114 +1,105 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
 import MenuItem from "@mui/material/MenuItem";
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import Select from "@mui/material/Select";
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import { useState, useEffect, useContext } from "react";
-import ReportContext from '../ReportContext';
-import Paper from '@mui/material/Paper';
+import ReportContext from "../ReportContext";
+import Paper from "@mui/material/Paper";
 import LogoImg from "../logo.png";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledSelect = styled(Select)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from Icon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "20ch",
     },
   },
 }));
 
 export default function HeaderBar() {
-    // eslint-disable-next-line no-unused-vars
-    const [_, setCurrentReport] = useContext(ReportContext);
-    const [transactionReports, setTransactionReports] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [_, setCurrentReport] = useContext(ReportContext);
+  const [transactionReports, setTransactionReports] = useState([]);
 
-    useEffect(() => {
-        getTransactionReports();
+  useEffect(() => {
+    getTransactionReports();
 
-        async function getTransactionReports() {
-            fetch("https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/filemap.xml")
-                .then((response) => response.text())
-                .then((response) => {
-                const parser = new DOMParser();
-                const xml = parser.parseFromString(response, "text/xml");
-                const results = [].slice
-                    .call(xml.getElementsByTagName("Key"))
-                    .filter((key) => key.textContent.includes(".json"));
-                setTransactionReports(results.map((file) => file.textContent));
-            });
-        }
-    }, []);
+    async function getTransactionReports() {
+      fetch("https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/filemap.xml")
+        .then((response) => response.text())
+        .then((response) => {
+          const parser = new DOMParser();
+          const xml = parser.parseFromString(response, "text/xml");
+          const results = [].slice
+            .call(xml.getElementsByTagName("Key"))
+            .filter((key) => key.textContent.includes(".json"));
+          setTransactionReports(results.map((file) => file.textContent));
+        });
+    }
+  }, []);
 
   return (
     <Paper elevation={5}>
-        <Box sx={{ flexGrow: 1 }} >
-            <AppBar position="static">
-                <Toolbar>
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="div"
-                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-                >
-                    <img src={LogoImg} alt="Logo" style={{ verticalAlign:"middle"}}/>
-                </Typography>
-                <Search>
-                    <SearchIconWrapper>
-                    <CalendarMonthIcon />
-                    </SearchIconWrapper>
-                    <StyledSelect 
-                        inputProps={{ 'aria-label': 'search' }}
-                        onChange={(event) => {
-                            setCurrentReport(event.target.value);
-                        }}
-                        >
-                        {transactionReports.map((file) => (
-                            <MenuItem
-                                key={file}
-                                value={file}
-                            >
-                                {file.replace("data/transaction_report_for_", "").replace(".json", "").replace(/_/g, "-")}
-                            </MenuItem>
-                        ))}
-                    </StyledSelect>
-                </Search>
-                </Toolbar>
-            </AppBar>
-        </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
+              <img src={LogoImg} alt="Logo" style={{ verticalAlign: "middle" }} />
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <CalendarMonthIcon />
+              </SearchIconWrapper>
+              <StyledSelect
+                onChange={(event) => {
+                  setCurrentReport(event.target.value);
+                }}
+              >
+                {transactionReports.map((file) => (
+                  <MenuItem key={file} value={file}>
+                    {file.replace("data/transaction_report_for_", "").replace(".json", "").replace(/_/g, "-")}
+                  </MenuItem>
+                ))}
+              </StyledSelect>
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </Paper>
   );
 }
